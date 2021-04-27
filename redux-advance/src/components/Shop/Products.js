1b8e0516115d981f52ce73deafa5c6c1c744eb70
shubhamgm.iprogrammer <shubhamgm.iprogrammer@gmail.com>
2021-04-27 19:26:14 +0530

    advance redux and basics redux

---
+++ redux-advance/src/components/Shop/Products.js
@@ -0,0 +1,37 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+
+const DUMMY_PRODUCTS = [ {
+  id:"p1",
+  price:6,
+  title:"my first Book",
+  description:"the first book i w=ever wrote", 
+},{
+  id:"p2",
+  price:10,
+  title:"my second Book",
+  description:"the second book i w=ever wrote", 
+}]
+
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map(product =>  (
+        <ProductItem
+           key={product.id}
+           id={product.id}
+          title= {product.title}
+          price={product.price}
+          description={product.description}
+        />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
---
+++ redux-advance/src/store/cart-actions.js
@@ -0,0 +1,79 @@
+import { uiActions } from "./ui-slice";
+import {cartActions} from "./cart-slice"
+
+
+export const fetchCartData = () => {
+    return  async(dispatch) => {
+      
+        const fetchData = async() => {
+               const response = await fetch('https://fir-redux-e30a7-default-rtdb.firebaseio.com/cart.json')
+        
+
+        if(!response.ok) {
+            throw new Error("could not fetch data")
+        }
+
+        const data = await response.json()
+
+        return data;
+    }
+
+
+    try {
+       const cartData = await  fetchData();
+       dispatch(cartActions.replaceCart(cartData))
+    }catch (error){
+        dispatch({
+            status: 'error',
+            title: 'Error!',
+            message: 'Sending cart data failed!',
+        })
+    }
+}
+}
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+  
+      const sendRequest = async () => {
+        const response = await fetch(
+          'https://fir-redux-e30a7-default-rtdb.firebaseio.com/cart.json',
+          {
+            method: 'PUT',
+            body: JSON.stringify(cart),
+          }
+        );
+  
+        if (!response.ok) {
+          throw new Error('Sending cart data failed.');
+        }
+      };
+  
+      try {
+        await sendRequest();
+  
+        dispatch(
+          uiActions.showNotification({
+            status: 'success',
+            title: 'Success!',
+            message: 'Sent cart data successfully!',
+          })
+        );
+      } catch (error) {
+        dispatch(
+          uiActions.showNotification({
+            status: 'error',
+            title: 'Error!',
+            message: 'Sending cart data failed!',
+          })
+        );
+      }
+    };
+  };
